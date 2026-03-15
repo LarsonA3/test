f3a038b778ec6984a556b7911ff8de137a374121
LarsonA3 <glitchy721@gmail.com>
2026-03-15 15:07:12 -0500

    Fixed LR movement
    added max x values that werent there before

--- test/Assets/Scripts/Player.cs
+++ test/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+//using static UnityEngine.RuleTile.TilingRuleOutput; //this was causing error
 
 public class Player : MonoBehaviour {
   // set in inspector
@@ -12,6 +13,7 @@ public class Player : MonoBehaviour {
 
 
   private const float Y_LIMIT = 4.6f;
+  private const float X_LIMIT = 9.92f;
 
   private void Start() {
     inputActions = new();
@@ -19,28 +21,34 @@ public class Player : MonoBehaviour {
     inputActions.Standard.Enable();
   }
 
-  private void Update() {
-    if (inputActions.Standard.Fire.WasPressedThisFrame()) {
-      GameObject bulletObj = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-    }
+    private void Update()
+    {
+        if (inputActions.Standard.Fire.WasPressedThisFrame())
+        {
+            GameObject bulletObj = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        }
 
 
-    if (inputActions.Standard.MoveUp.IsPressed()) {
-      this.transform.Translate(Vector3.up * speed * Time.deltaTime);
-    }
-    if (inputActions.Standard.MoveDown.IsPressed()) {
-      this.transform.Translate(Vector3.down * speed * Time.deltaTime);
-    }
-    if (inputActions.Standard.MoveLeft.IsPressed()) {
-      this.transform.Translate(Vector3.left * LRspeed * Time.deltaTime);
-    }
-    if (inputActions.Standard.MoveRight.IsPressed()) {
-      this.transform.Translate(Vector3.right * LRspeed * Time.deltaTime);
-    }
+        if (inputActions.Standard.MoveUp.IsPressed())
+        {
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (inputActions.Standard.MoveDown.IsPressed())
+        {
+            this.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (inputActions.Standard.MoveLeft.IsPressed())
+        {
+            this.transform.Translate(Vector3.left * LRspeed * Time.deltaTime);
+        }
+        if (inputActions.Standard.MoveRight.IsPressed())
+        {
+            this.transform.Translate(Vector3.right * LRspeed * Time.deltaTime);
+        }
 
 
 
-    if (this.transform.position.y > Y_LIMIT)
+        if (this.transform.position.y > Y_LIMIT)
         {
             this.transform.position = new Vector3(transform.position.x, Y_LIMIT);
         }
@@ -48,9 +56,23 @@ public class Player : MonoBehaviour {
         {
             this.transform.position = new Vector3(transform.position.x, -Y_LIMIT);
         }
-  }
 
-  private void OnCollisionExit2D(Collision2D collision)
+
+        if (this.transform.position.x > X_LIMIT)
+        {
+            this.transform.position = new Vector3(X_LIMIT, transform.position.y, 0);
+        }
+        else if (this.transform.position.x < -X_LIMIT)
+        {
+            this.transform.position = new Vector3(-X_LIMIT, transform.position.y, 0);
+        }
+
+
+
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
   {
         if (collision.gameObject.CompareTag("EnemyPhysicalHit"))
         {
