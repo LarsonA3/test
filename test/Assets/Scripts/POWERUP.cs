88099a6af3cb11bbc3cbd117900532d591abe593
LarsonA3 <glitchy721@gmail.com>
2026-03-16 14:42:16 -0500

    IGNORE
    just some psuedocode for powerups

---
+++ test/Assets/Scripts/POWERUP.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class POWERUP : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        //check object tag to determine what powerup it is
+        //set timer times depending on what it is
+        //call to methods in game to do stuff
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //go left
+        
+    }
+
+
+    //upon player touching give powerup (big if else) , set timer, hide sprite then freeze, and  once timer is done call to game to end, destroy whole object
+}
